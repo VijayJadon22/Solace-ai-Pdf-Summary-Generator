b973d5bd0489aa0520088a39bbbbf3f39a147f8a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-09-11 23:50:06 +0530

    Creating Header or Navbar with all the navigation links

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <header className="bg-red-600">Header</header>;
+  return (
+    <nav className="container bg-red-500 flex justify-between items-center">
+      <div>
+        <Link href={"/"}>Solace ai</Link>
+      </div>
+      <div>
+        <Link href={"/#pricing"}>Pricing</Link>
+      </div>
+      <div>
+        <Link href={"/sign-in"}>Sign In</Link>
+      </div>
+    </nav>
+  );
 }
