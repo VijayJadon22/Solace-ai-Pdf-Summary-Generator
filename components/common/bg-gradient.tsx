7059f82edbcef4a52680a94053f1afe5cd98d067
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-09-16 19:31:43 +0530

    Configured bg-gradient for the app

--- components/common/bg-gradient.tsx
+++ components/common/bg-gradient.tsx
@@ -1,30 +1,24 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-export default function BgGradient({
-  children,
-  className,
-}: {
-  children?: React.ReactNode;
-  className?: string;
-}) {
+const BgGradient = ({ className }: { className?: string }) => {
   return (
     <div
-      aria-hidden="true"
-      className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-30"
+      aria-hidden
+      className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-32"
     >
       <div
         style={{
           clipPath:
-            "polygon(50% 0%, 61% 35%, 98% 35%,68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)",
+            "polygon(50% 0% 61% 35% 98% 35% 68% 57% 79% 91% 50% 70% 21% 91% 32% 57% 2% 35% 39% 35%)",
         }}
         className={cn(
-          "relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-br from-emerald-500 via-teal-500 to-cyan-500 opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72rem]",
+          "relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-br from-emerald-500 via-teal-500 to-cyan-500 opacity-20 sm:left-[cal(50%-30rem)] sm:w-[72rem]",
           className
         )}
-      >
-        {children}
-      </div>
+      />
     </div>
   );
-}
+};
+
+export default BgGradient;
